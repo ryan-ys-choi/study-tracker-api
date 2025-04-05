8a123aa00611078ed4bea98851115c5e0271be7a
ryan-ys-choi <104918388+ryan-ys-choi@users.noreply.github.com>
2025-04-04 22:29:14 -0700

    running Flask web application

--- app/__init__.py
+++ app/__init__.py
@@ -3,9 +3,9 @@ import os
 from flask import Flask
 
 def create_app(test_config=None):
-    # create and configure the app
+    # create the Flask instance and configure the app
     app = Flask(__name__, instance_relative_config=True)
-    # raw SQL approach
+    # raw SQL approach: set default configuration that the app will use
     app.config.from_mapping(
         SECRET_KEY='dev',
         DB_HOST='localhost',
@@ -22,4 +22,7 @@ def create_app(test_config=None):
     
     # a simple page that says hello
     @app.route('/hello')
+    def hello():
+        return 'Hello, World'
     
+    return app
